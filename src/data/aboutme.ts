5754e88ac727b6c3c1822809694e43c4f9e10215
Masanori Suganuma <ykhmprsi1029@gmail.com>
2025-02-04 18:37:53 +0900

    Update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -18,22 +18,22 @@ export interface AboutMe {
 }
 
 export const aboutMe: AboutMe = {
-  name: "Jane R. Smith",
-  title: "Ph.D. Candidate",
-  institution: "Stanford University",
+  name: "Masanori Suganuma",
+  title: "Research Scientist",
+  institution: "Sakana AI",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
+    "I'm a research scientist working at Sakana AI in Tokyo. My research focuses on developing good vision and language models.",
+  email: "msuganuma[at]sakana.ai",
   imageUrl:
     "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
+  googleScholarUrl: "https://scholar.google.co.jp/citations?user=NpWGfwgAAAAJ&hl=ja&oi=ao",
+  githubUsername: "sg-nm",
+  linkedinUsername: "masanori-suganuma-267928171",
+  twitterUsername: "wkpeco",
   blogUrl: "https://",
   cvUrl: "https://",
-  institutionUrl: "https://www.stanford.edu",
+  // institutionUrl: "https://www.stanford.edu",
   // altName: "",
   // secretDescription: "I like dogs.",
 };
